1be31e3ee8effbb309211f8ce3e16db3560e4cef
Abdul Hanan <abdulhanan347@gmail.com>
2023-08-03 18:06:39 -0400

    created directory structure

--- src/server.js
+++ src/server.js
@@ -2,25 +2,25 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+// const userRoutes = require('./routes/userRoutes');
+
 
 const app = express();
 const port = 5000; // You can use any port number you prefer
-
 // Middleware
 app.use(bodyParser.json());
 
-// PostgreSQL Configuration
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'emailOs',
-  password: 'password',
-  port: 5432, // Default PostgreSQL port
-});
+// Routes
+// app.use('/api/users', userRoutes);
+
+
+
 
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  
 });
+
+
